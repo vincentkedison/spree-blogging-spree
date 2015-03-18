6558976c059f82415e428dc2ed922b02d0abe61b
j540 <james@tokyowheel.com>
2015-03-18 20:01:23 +0700

    change admin icon to comment

--- app/overrides/add_blog_admin_menu_tab.rb
+++ app/overrides/add_blog_admin_menu_tab.rb
@@ -1,7 +1,7 @@
 Deface::Override.new(:virtual_path => "spree/layouts/admin",
                      :name => "blog_admin_tabs",
                      :insert_bottom => "#main-sidebar",
-                     :text => "<% if Spree.user_class && can?(:admin, Spree::BlogEntry) %><ul class='nav nav-sidebar'><%= tab Spree.t(:BLOG), url: spree.admin_blog_entries_path, icon: 'file' %></ul><% end %>",
+                     :text => "<% if Spree.user_class && can?(:admin, Spree::BlogEntry) %><ul class='nav nav-sidebar'><%= tab Spree.t(:BLOG), url: spree.admin_blog_entries_path, icon: 'comment' %></ul><% end %>",
                      :disabled => false)
 
 
