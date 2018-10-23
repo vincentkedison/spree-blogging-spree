e99df5d8d8376612808336ba33d47fd1b7e88ea3
Viacheslav Ptsarev <leemour@gmail.com>
2018-10-23 15:25:34 +0300

    Add indexes to BlogEntries

---
+++ db/migrate/20181022165517_add_indexes_to_blog_entries.rb
@@ -0,0 +1,13 @@
+class AddIndexesToBlogEntries < ActiveRecord::Migration[4.2]
+  def up
+    add_index :spree_blog_entries, :title
+    add_index :spree_blog_entries, :created_at
+    add_index :spree_blog_entries, :visible
+  end
+
+  def down
+    remove_index :spree_blog_entries, :title
+    remove_index :spree_blog_entries, :created_at
+    remove_index :spree_blog_entries, :visible
+  end
+end
