f5c26c5f1470ce373bf0f9fde30706d99da6bbea
Viacheslav Ptsarev <leemour@gmail.com>
2018-10-23 16:58:52 +0300

    Add index for published_at

--- db/migrate/20181022165517_add_indexes_to_blog_entries.rb
+++ db/migrate/20181022165517_add_indexes_to_blog_entries.rb
@@ -2,12 +2,14 @@ class AddIndexesToBlogEntries < ActiveRecord::Migration[4.2]
   def up
     add_index :spree_blog_entries, :title
     add_index :spree_blog_entries, :created_at
+    add_index :spree_blog_entries, :published_at
     add_index :spree_blog_entries, :visible
   end
 
   def down
     remove_index :spree_blog_entries, :title
     remove_index :spree_blog_entries, :created_at
+    remove_index :spree_blog_entries, :published_at
     remove_index :spree_blog_entries, :visible
   end
 end
