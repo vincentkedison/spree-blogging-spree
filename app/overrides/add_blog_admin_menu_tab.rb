212e220fbb1c18a12d6ed15602cb726bdb8ab1fc
Vincent K Edison <vk@luxodev.com>
2019-05-22 17:37:14 +0700

    fix

--- app/overrides/add_blog_admin_menu_tab.rb
+++ app/overrides/add_blog_admin_menu_tab.rb
@@ -7,5 +7,4 @@
 Deface::Override.new(   virtual_path: 'spree/layouts/admin',
     name: 'blog_admin_sidebar_menu',
     insert_bottom: '#main-sidebar',
-    partial: 'spree/admin/shared/blog_sidebar_menu',
-    original: '120a57c9d1454cc9a61d7e32d4099622d713af24')
+    partial: 'spree/admin/shared/blog_sidebar_menu')
